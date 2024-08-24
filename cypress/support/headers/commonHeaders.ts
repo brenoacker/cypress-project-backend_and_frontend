27df0c7c6f0efcbb09f8643d888d6b430bc8c412
brenoacker <brenoacker@gmail.com>
2024-08-24 10:42:54 -0300

    adding a common header function

---
+++ cypress/support/headers/commonHeaders.ts
@@ -0,0 +1,7 @@
+export default function commonHeaders() {
+    return {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Accept-Encoding': 'gzip, deflate',
+    };
+}
